deef7e7a139bffa1a28dd75211261e1abf0e242f
ryane joe <ryane@rcorp.enterprises>
2026-01-27 21:03:42 +0000

    Add debug logging to verify fresh build
    Testing deployment - site showing blank page despite clean build locally

--- web/src/App.js
+++ web/src/App.js
@@ -8,6 +8,7 @@ import './index.css';
  * TODO: Add routing when auth pages are built
  */
 function App() {
+  console.log('Vicarity App Loading - Build:', process.env.NODE_ENV);
   return <LandingPage />;
 }
 
