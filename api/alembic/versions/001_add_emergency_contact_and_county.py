a60d7d2091fd633a0637a4b1f0cb47fce61a6560
ryane joe <ryane@rcorp.enterprises>
2026-02-06 21:07:57 +0000

    Fix migration 001 revision ID to match references

--- api/alembic/versions/001_add_emergency_contact_and_county.py
+++ api/alembic/versions/001_add_emergency_contact_and_county.py
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '001'
+revision = '001_add_emergency_contact_and_county'
 down_revision = None
 branch_labels = None
 depends_on = None
